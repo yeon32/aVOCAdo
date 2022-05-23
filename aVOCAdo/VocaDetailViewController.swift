690a57aa6d2457791cef59f402dcdf8446e1ca45
yeon32 <32jyeon@gamil.com>
2022-05-23 21:35:09 +0900

    단어 리스트에 추가 단어 보이기

--- aVOCAdo/VocaDetailViewController.swift
+++ aVOCAdo/VocaDetailViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class VocaDetailViewController: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var meaningTextField: UITextField!
+    
+    
+    var voca: Voca?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    
+    @IBAction func editButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
+    }
+    
 
 
 }
--- aVOCAdo/WriteVocaViewController.swift
+++ aVOCAdo/WriteVocaViewController.swift
@@ -59,7 +59,7 @@ class WriteVocaViewController: UIViewController, UITextFieldDelegate {
         self.view.endEditing(true)
     }
     
-    //hello
+    
     
 }
 
